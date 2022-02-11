f46fa15a47f3c372970597977a4d28ea206e529a
nazavod777 <91217742+nazavod777@users.noreply.github.com>
2022-02-12 02:12:39 +0300

    upd 12.02.22
    - Заменил сервис временной почты, ибо предыдущий забанили

--- legionNETWORK.py
+++ legionNETWORK.py
@@ -1,4 +1,5 @@
 import cloudscraper
+import requests
 from bs4 import BeautifulSoup
 from ctypes import windll
 from gc import collect
@@ -13,6 +14,8 @@ from threading import Thread
 from time import sleep
 from urllib3 import disable_warnings
 from uuid import uuid4
+from pyuseragents import random as random_useragent
+from json import loads
 
 disable_warnings()
 system("cls")
@@ -40,15 +43,22 @@ def mainth():
 		try:
 			udid = str(uuid4())
 			scraper = cloudscraper.create_scraper()
+			session = requests.Session()
+			session.headers.update({'user-agent': random_useragent(), 'Accept': 'application/json, text/plain, */*', 'Referer': 'https://temprmail.com/'})
 			if use_proxy in ('y', 'Y'):
 				proxy_str = take_proxy()
 				scraper.proxies.update({'http': f'{proxy_type}://{proxy_str}', 'https': f'{proxy_type}://{proxy_str}'})
 			scraper.headers.update({'Content-Type': 'application/json', 'cf-visitor': 'https', 'User-Agent': 'Legion/5.2 CFNetwork/1209 Darwin/20.2.0', 'Connection': 'keep-alive', 'Accept': 'application/json, text/plain, */*', 'Accept-Language': 'ru', 'x-forwarded-proto': 'https', 'Accept-Encoding': 'gzip, deflate, br'})
 
+			r = session.post('https://api.temprmail.com/v1/emails')
+			email = loads(r.text)['email']
+			checkmailsurl = loads(r.text)['emails_json_url']
+			logger.info(f'Email {email} успешно зарегистрирован')
+
 			username = generate_username()[0]+''.join([choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789' if i != 5 else 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for i in range(5)])
 			nameOfAcc = get_first_name()
 			password = str(randint(0,9))+''.join([choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789' if i != 25 else 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for i in range(25)])
-			body = {"password":str(password)+"!","email":str(username)+"@oosln.com","name":str(nameOfAcc),"udid":udid,"referralCode":str(refferalCode)}
+			body = {"password":str(password)+"!","email":email,"name":str(nameOfAcc),"udid":udid,"referralCode":str(refferalCode)}
 			r = scraper.post('https://api.legionnetwork.io/api1/user/create', json = body)
 
 			if r.status_code == 200:
@@ -56,12 +66,9 @@ def mainth():
 
 				logger.info(f'Ожидаю письмо для {username}')
 				for i in range(13):
-					r = get(f"https://www.1secmail.com/api/v1/?action=getMessages&login={username}&domain=oosln.com")
-					r_json = r.json()
-					if len(r_json) > 0:
-						latest_mail = r_json[0].get('id')
-						req = get(f"https://www.1secmail.com/mailbox/?action=mailBody&id={latest_mail}&login={username}&domain=oosln.com")
-						urlVerify = BeautifulSoup(req.text,'html').find_all("a", href=True)[1].get("href")
+					r = scraper.get(checkmailsurl)
+					if 'Verification Link' in r.text:
+						msgid = loads(r.text)[0]['hash_id']
 						logger.success(f'Письмо для {username} успешно получено')
 						break
 					else:
@@ -69,6 +76,10 @@ def mainth():
 							raise Exception('email_timeout')
 						else:
 							sleep(3)
+
+				r = session.get(f'https://tempremail-assets.s3.us-east-1.amazonaws.com/emails/{msgid}.json')
+				urlVerify = r.text.split('please complete your registration and confirm your email address by clicking the button below')[-1].split('href=\\"')[1].split('\\" target=')[0].replace('\\/', '/')
+
 				for i in range(30):
 					r = scraper.get(urlVerify).url
 					if 'token=' in r:
@@ -108,8 +119,9 @@ def cleaner():
 		clear()
 		collect()
 
-clear()
-for _ in range(threads):
-	Thread(target=mainth).start()
+if __name__ == '__main__':
+	clear()
+	for _ in range(threads):
+		Thread(target=mainth).start()
 
-Thread(target=cleaner, daemon=True).start()
+	Thread(target=cleaner, daemon=True).start()
